3a56981d111b49369cbf3a259a0d818bc028b3aa
Andre <afowlk1@gmail.com>
2020-10-01 15:28:05 -0400

    -

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import slugify from 'slugify';
 import './App.css';
 import ComponentList from './ComponentList/ComponentList'
+import ShoppingCart from './ShoppingCart/ShoppingCart'
 
 // This object will allow us to
 // easily convert numbers into US dollar values
@@ -42,25 +43,7 @@ export default class App extends Component {
 
   render() {
 
-    const summary = Object.keys(this.state.selected).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const selectedOption = this.state.selected[feature];
-
-      return (
-        <div className="summary__option" key={featureHash}>
-          <div className="summary__option__label">{feature} </div>
-          <div className="summary__option__value">{selectedOption.name}</div>
-          <div className="summary__option__cost">
-            {USCurrencyFormat.format(selectedOption.cost)}
-          </div>
-        </div>
-      );
-    });
-
-    const total = Object.keys(this.state.selected).reduce(
-      (acc, curr) => acc + this.state.selected[curr].cost,
-      0
-    );
+    
 
     return (
       <div className="App">
@@ -74,21 +57,15 @@ export default class App extends Component {
             selected={this.state.selected}
             updateFeature={this.updateFeature}
           />
-          {/* <form className="main__form">
-            <h2>Customize your laptop</h2>
-            {features}
-          </form> */}
+          
+          {/* ShoppingCart Section */}
 
-          <section className="main__summary">
-            <h2>Your cart</h2>
-            {summary}
-            <div className="summary__total">
-              <div className="summary__total__label">Total</div>
-              <div className="summary__total__value">
-                {USCurrencyFormat.format(total)}
-              </div>
-            </div>
-          </section>
+          <ShoppingCart 
+            selected={this.state.selected}
+            
+
+          />
+          
         </main>
       </div>
     );
